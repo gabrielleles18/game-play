fb005359def6a8e27a158fdeeeaa08974077c317
gabrielleles18 <gabriel.leles18@gmail.com>
2021-08-31 22:37:25 -0300

    Atualizacao

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useState} from "react";
+import {View} from "react-native";
+
+import {ButtonAdd} from "../../components/ButtonAdd";
 import {Profile} from "../../components/Profile";
+import {CategorySelect} from "../../components/CategorySelect";
 
 import {styles} from "./styles";
 
 export function Home() {
+    const [category, setCategory] = useState('');
+
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+
     return (
         <View>
 
             <View style={styles.header}>
                 <Profile/>
+                <ButtonAdd/>
             </View>
 
+            <View>
+                <CategorySelect
+                    categorySelected={category}
+                    setCategory={handleCategorySelect}
+                />
+            </View>
         </View>
     )
 }
